c8cfcf5012da4e1ba0396066cfa1bf781fd0b749
LukeGiles3 <lukegiles@Lukes-MacBook-Pro.local>
2020-03-04 10:06:32 -0700

    styling/bgimage

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-function newItem() {
-    var li = document.createElement("LI");
-    $(li).addClass("list-group-item");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-    }
-
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("BUTTON");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+class User {
+  constructor(toDoLists) {
+    this.toDoLists = toDoLists;
   }
 }
+
+class ToDoList {
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+
+  }
+}
+
+class Task {
+  constructor(notDone, done, edit) {
+    this.notDone = notDone;
+    this.done = done;
+    this.edit = edit;
+  }
+}
+// function newItem() {
+//     var li = document.createElement("LI");
+//     $(li).addClass("list-group-item");
+//     var inputValue = document.getElementById("myInput").value;
+//     var t = document.createTextNode(inputValue);
+//     li.appendChild(t);
+//     if (inputValue === '') {
+//       alert("You must write something!");
+//     } else {
+//       document.getElementById("myUL").appendChild(li);
+//     }
+//   }
+    // document.getElementById("myInput").value = "";
+    // var myNodelist = document.getElementsByTagName("LI");
+    // var i;
+    // for (i = 0; i < myNodelist.length; i++) {
+    //   var span = document.createElement("SPAN");
+    //   var txt = document.createTextNode("\u00D7");
+    //   span.className = "close";
+    //   span.appendChild(txt);
+    //   myNodelist[i].appendChild(span);
+    // }
+    // }
+
+// Create a "close" button and append it to each list item
+
+
+// Click on a close button to hide the current list item
