be3466d511fa3937a52b6e0173e3320049303875
LukeGiles3 <lukegiles@Lukes-MacBook-Pro.local>
2020-03-12 09:17:19 -0600

    layout/look

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+function newList() {
+  
+  
+}
+
 function todoList() {
   if(document.getElementById("todoInput").value == '' || document.getElementById("todoInput").value == null) {
-    alert("Doing nothing is something right?")
+    alert("Doing nothing is something right? :D")
   } else {
     let item = document.getElementById("todoInput").value
     let text = document.createTextNode(item)
@@ -10,79 +15,3 @@ function todoList() {
     document.getElementById("todoList").appendChild(newitem)
   }
 }
-
-
-
-
-
-
-
-
-
-
-// class User {
-//   constructor(toDoLists) {
-//     this.toDoLists = toDoLists;
-//   }
-//   newList() {
-//     document.createElement('input')
-//   }
-// }
-
-// class ToDoList {
-//   constructor(selectedHtmlElement) {
-//     this.name = [];
-//     this.notDone = [];
-//     this.done = [];
-//     this.selectedHtmlElement = selectedHtmlElement || document.body;
-//     this.tasks = JSON.parse(window.localStorage.getItem("tasks")) || [];
-//   }
-
-//   addTask(text) {
-//     if (text == '' || text == null) {
-//       alert("Doing nothing is something, right?")
-//     } else {
-//       this.tasks.push(new Task(text))
-//       this.saveTask
-//     }
-//     this.render(this.tasks)
-//   }
-//   saveTask() {
-//     window.localStorage.setItem("tasks", JSON.stringify(this.tasks));
-//   }
-// }
-
-// class Task {
-//   constructor(text) {
-//     this.text = text;
-//     this.isCompleted = false;
-//   }
-// }
-// function newItem() {
-//     var li = document.createElement("LI");
-//     $(li).addClass("list-group-item");
-//     var inputValue = document.getElementById("myInput").value;
-//     var t = document.createTextNode(inputValue);
-//     li.appendChild(t);
-//     if (inputValue === '') {
-//       alert("You must write something!");
-//     } else {
-//       document.getElementById("myUL").appendChild(li);
-//     }
-//   }
-    // document.getElementById("myInput").value = "";
-    // var myNodelist = document.getElementsByTagName("LI");
-    // var i;
-    // for (i = 0; i < myNodelist.length; i++) {
-    //   var span = document.createElement("SPAN");
-    //   var txt = document.createTextNode("\u00D7");
-    //   span.className = "close";
-    //   span.appendChild(txt);
-    //   myNodelist[i].appendChild(span);
-    // }
-    // }
-
-// Create a "close" button and append it to each list item
-
-
-// Click on a close button to hide the current list item
--- test.js
+++
@@ -1,7 +0,0 @@
-function todoList() {
-    let item = document.getElementById("todoInput").value
-    let text = document.createTextNode(item)
-    let newitem = document.createElement("li")
-    newitem.appendChild(text)
-    document.getElementById("todoList").appendChild(newitem)
-}
